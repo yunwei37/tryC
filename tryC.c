2ada3a825370a4f86502645d4c17410b07dee330
云微 <34985212+yunwei37@users.noreply.github.com>
2020-05-04 00:03:24 +0800

    nearly finish

--- tryC.c
+++ tryC.c
@@ -2,52 +2,48 @@
 #include <float.h>
 
 #define POOLSIZE  (256 * 1024)  // arbitrary size
-#define SYMTABSIZE (1024*8)
-#define MAXNAMESIZE 32
-#define STRBSIZE 1024
+#define SYMTABSIZE (1024*8)     // size of the symbol table stack
+#define MAXNAMESIZE 32          // size of variable/function name
 
-typedef struct symStruct {
-    int type;
-    char name[MAXNAMESIZE];
-    double value;
+/* this structure represent a symbol store in a symbol table */
+typedef struct symStruct {  
+    int type;                  //the type of the symbol:  Num, Char, Str, Array, Func
+    char name[MAXNAMESIZE];    // record the symbol name
+    double value;              // record the symbol value, or the length of string or array
     union {
-        char* funcp;
-        double* list;
+        char* funcp;            // pointer to an array or an function
+        struct symStruct* list;
     } pointer;
-    int layerNum;
+    int levelNum;               // indicate the declare nesting level
 } symbol;
 symbol symtab[SYMTABSIZE];
-int symPointer = 0;
-int currentLayer = 0;
+int symPointer = 0;             // the symbol stack pointer
+int currentlevel = 0;           // current nesting level
 
-//int line = 0;   // line number
-char* src, * old_src;
+char* src, * old_src;           // the text process currently
 
 enum {
     debug, run
 };
-int compileState = 0;
+int compileState = run;          // current interpre state
 
-double return_val = 0;
+double return_val = 0;           // used for reserve the return value of function
 
-// tokens and classes (operators last and in precedence order)
+/* tokens and classes (operators last and in precedence order) */
 enum {
     Num = 128, Char, Str, Array, Func,
-    Else, If, Return, While, Print,
+    Else, If, Return, While, Print,Puts, Read,
     Assign, OR, AND, Equal, Sym, FuncSym, ArraySym, Void,
     Nequal, LessEqual, GreatEqual, Inc, Dec
 };
-int token;  // current token
+int token;                      // current token type
 union tokenValue {
-    symbol* ptr;
-    double val;
+    symbol* ptr;                // used when return a string or a symbol address for assignment
+    double val;                 // token value, for Char or Num
 } token_val;
 
-char tempStrBuffer[STRBSIZE];
-
+/*--------------- function declaration ---------------*/
 double function();
-double stblock();
-void eval();
 double statement();
 int boolOR();
 int boolAND();
@@ -58,13 +54,16 @@ double term();
 void match(int tk);
 void next();
 
+/* get the next token of the input string */
 void next() {
     char* last_pos;
 
     while (token = *src) {
         ++src;
         if (token == '\n') {                // a new line
-            //line++;
+            if(compileState == debug)       // if on debug mode, print the currnet process line
+                printf("%.*s",  src - old_src, old_src);
+            old_src = src;
         }
         else if (token == '#') {            // skip comments
             while (*src != 0 && *src != '\n') {
@@ -72,7 +71,7 @@ void next() {
             }
         }
         else if ((token >= 'a' && token <= 'z') || (token >= 'A' && token <= 'Z') || (token == '_')) {
-            last_pos = src - 1;
+            last_pos = src - 1;             // process symbols
             char nameBuffer[MAXNAMESIZE];
             nameBuffer[0] = token;
             while ((*src >= 'a' && *src <= 'z') || (*src >= 'A' && *src <= 'Z') || (*src >= '0' && *src <= '9') || (*src == '_')) {
@@ -87,11 +86,11 @@ void next() {
                         token_val.ptr = &symtab[i];
                         token = Sym;
                     }
-                    else if (symtab[i].type == Func) {
+                    else if (symtab[i].type == FuncSym) {
                         token_val.ptr = &symtab[i];
                         token = symtab[i].type;
                     }
-                    else if (symtab[i].type == Array) {
+                    else if (symtab[i].type == ArraySym) {
                         token_val.ptr = &symtab[i];
                         token = symtab[i].type;
                     }
@@ -102,15 +101,15 @@ void next() {
                 }
             }
             strcpy(symtab[symPointer].name, nameBuffer);
-            symtab[symPointer].layerNum = currentLayer;
+            symtab[symPointer].levelNum = currentlevel;
             symtab[symPointer].type = Void;
             token_val.ptr = &symtab[symPointer];
             symPointer++;
             token = Sym;
             return;
         }
-        else if (token >= '0' && token <= '9') {
-            token_val.val = token - '0';
+        else if (token >= '0' && token <= '9') {        // process numbers
+            token_val.val = (double)token - '0';
             while (*src >= '0' && *src <= '9') {
                 token_val.val = token_val.val * 10.0 + *src++ - '0';
             }
@@ -118,18 +117,19 @@ void next() {
                 src++;
                 int countDig = 1;
                 while (*src >= '0' && *src <= '9') {
-                    token_val.val = token_val.val + (*src++ - '0')/(10.0 * countDig++);
+                    token_val.val = token_val.val + ((double)(*src++) - '0')/(10.0 * countDig++);
                 }
             }
             token = Num;
             return;
         }
-        else if (token == '\'') {
-            token_val.val = *src;
+        else if (token == '\'') {               // parse char
+            token_val.val = *src++;
             token = Char;
+            src++;
             return;
         }
-        else if (token == '"' ) {
+        else if (token == '"' ) {               // parse string
             last_pos = src;
             char tval;
             int numCount = 0;
@@ -137,9 +137,14 @@ void next() {
                 src++;
                 numCount++;          
             }
-
-            src++;
-            token_val.ptr = last_pos;
+            if (*src) {
+                *src = 0;
+                token_val.ptr = malloc(sizeof(char) * numCount + 8);
+                strcpy(token_val.ptr, last_pos);
+                *src = token;
+                src++;
+            }
+            token = Str;
             return;
         }
         else if (token == '=') {            // parse '==' and '='
@@ -198,8 +203,8 @@ void next() {
             }
             return;
         }
-        else if (token == '+' || token == '-' || token == '*' || token == '/' || token == '!' || token == ';' || token == ',' ||
-            token == '(' || token == ')' || token == '{' || token == '}' || token == '>' || token == '<' || token == '=' || token == '[' || token == ']') {
+        else if ( token == '*' || token == '/'  || token == ';' || token == ',' ||
+            token == '(' || token == ')' || token == '{' || token == '}' ||  token == '[' || token == ']') {
             return;
         }
         else if (token == ' ' || token == '\t') {        }
@@ -214,8 +219,8 @@ void match(int tk) {
         next();
     }
     else {
-        printf("expected token: %d\n", tk);
-        //exit(-1);
+        printf("line %.*s:expected token: %d\n", src - old_src, old_src,  tk);
+        exit(-1);
     }
 }
 
@@ -268,10 +273,11 @@ double factor() {
     }
     else if (token == ArraySym) {
         symbol* ptr = token_val.ptr;
+        match(ArraySym);
         match('[');
         int index = (int)expression();
-        if (index > 0 && index < token_val.ptr->value) {
-            return token_val.ptr->pointer.list[index];
+        if (index >= 0 && index < ptr->value) {
+            temp = ptr->pointer.list[index].value;
         }
         match(']');
     }
@@ -325,6 +331,7 @@ int boolexp() {
         match(Equal);
         return temp == expression();
     }
+    return 0;
 }
 
 int boolAND() {
@@ -335,6 +342,7 @@ int boolAND() {
         val = val & boolexp();
         if (val == 0) return 0;
     }
+    return val;
 }
 
 int boolOR() {
@@ -344,23 +352,38 @@ int boolOR() {
         if (val == 1)    return 1;         // short cut
         val = val | boolAND();
     }
+    return val;
 }
 
 void skipStatments() {
-    match('{');
-    while (token && token != '}')
-        next();
+    if(token == '{')
+        token = *src++;
+    int count = 0;
+    while (token && !(token == '}' && count == 0)) {
+        if (token == '}') count++;
+        if (token == '{') count--;
+        token = *src++;
+    }
     match('}');
 }
 
 double statement() {
-    if (token == If) {
+    if (token == '{') {
+        match('{');
+        while (token != '}') {
+            if (DBL_MAX == statement()) {
+                return DBL_MAX;
+            }
+        }
+        match('}');
+    }
+    else if (token == If) {
         match(If);
         match('(');
         int boolresult = boolOR();
         match(')');
         if (boolresult) {
-            if (DBL_MAX == stblock()) {
+            if (DBL_MAX == statement()) {
                 return DBL_MAX;
             }
         }
@@ -368,7 +391,7 @@ double statement() {
         if (token == Else) {
             match('Else');
             if (!boolresult) {
-                if (DBL_MAX == stblock()) {
+                if (DBL_MAX == statement()) {
                     return DBL_MAX;
                 }
             }
@@ -388,29 +411,45 @@ double statement() {
             boolresult = boolOR();
             match(')');
             if (boolresult) {
-                if (DBL_MAX == stblock()) {
+                if (DBL_MAX == statement()) {
                     return DBL_MAX;
                 }
             }
             else skipStatments();
         }while (boolresult);
     }
-    else if (token == Sym) {
+    else if (token == Sym || token == ArraySym) {
         symbol* s = token_val.ptr;
-        match(Sym);
-        match('=');
-        if (token == Str) {
-            s->pointer.funcp = token_val.ptr;
-            s->type = Str;
+        int tktype = token;
+        int index;
+        match(tktype);
+        if (tktype == ArraySym) {
+            match('[');
+            index = expression();
+            match(']');
+            match('=');
+            if (index >= 0 && index < s->value) {
+                s->pointer.list[index].value = expression();
+            }
         }
-        else if (token == Char) {
-            s->value = token_val.val;
-            s->type = Char;
+        else {
+            match('=');
+            if (token == Str) {
+                s->pointer.funcp = (char*)token_val.ptr;
+                s->type = Str;
+                match(Str);
+            }
+            else if (token == Char) {
+                s->value = token_val.val;
+                s->type = Char;
+                match(Char);
+            }
+            else {
+                s->value = expression();
+                s->type = Num;
+            }
         }
-        else{
-            s->value = expression();
-            s->type = Num;
-        }
+        match(';');
     }
     else if (token == Array) {
         match(Array);
@@ -419,80 +458,86 @@ double statement() {
         match('(');
         int length = (int)expression();
         match(')');
-        s->pointer.list = malloc(sizeof(symbol) * length + 1);
+        s->pointer.list = (double*)malloc(sizeof(struct symStruct) * length + 1);
+        for (int i = 0; i < length; ++i)
+            s->pointer.list[i].type = Num;
         s->value = length;
-        s->type = Array;
+        s->type = ArraySym;
+        match(';');
     }
     else if (token == Func) {
         match(Func);
         symbol* s = token_val.ptr;
-        s->type = Func;
-        s->pointer.funcp = src;
+        s->type = FuncSym;
         match(Sym);
+        s->pointer.funcp = src;
+        s->value = token;
         skipStatments();
+        match(';');
     }
     else if (token == Return) {
         match(Return);
         match('(');
         return_val = expression();
         match(')');
+        match(';');
         return DBL_MAX;
     }
-    else if (token == Print) {
-        match(Print);
+    else if (token == Print || token == Read || token == Puts) {
+        int func = token;
+        double temp;
+        match(func);
         match('(');
-        double temp = expression();
+        switch (func) {
+        case Print: 
+            temp = expression();
+            printf("%lf\n", temp);
+            break;
+        case Puts: 
+            printf("%s\n", (char*)token_val.ptr);
+            match(Str);
+            break;
+        case Read:
+            scanf("%lf", &token_val.ptr->value);
+            token_val.ptr->type = Num;
+            match(Sym);
+        }
         match(')');
-        printf("%lf\n", temp);
+        match(';');
     }
     return 0;
 }
 
-double stblock() {
-    match('{');
-    while (token != '}') {
-        if (DBL_MAX == statement()) {
-            return DBL_MAX;
-        }
-        match(';');
-    }
-    match('}');
-}
-
-void eval() {
-    next();
-    while (token != 0) {
-        statement();
-        match(';');
-    }
-}
-
 double function() {
-    currentLayer++;
+    currentlevel++;
 
-    char* StartPos = src;
-    char* StartOldPos = old_src;
     symbol* s = token_val.ptr;
     match(FuncSym);
     match('(');
     while (token != ')') {
         symtab[symPointer] = *token_val.ptr;
         strcpy(symtab[symPointer].name, token_val.ptr->name);
-        symtab[symPointer].layerNum = currentLayer;
+        symtab[symPointer].levelNum = currentlevel;
         symPointer++;
         match(Sym);
-        match(',');
+        if (token == ',')
+            match(',');
     }
     match(')');
+    char* startPos = src;
+    char* startOldPos = old_src;
+    int startToken = token;
     old_src = src = s->pointer.funcp;
-    stblock();
-    src = StartPos;
-    old_src = StartOldPos;
+    token = (int)s->value;
+    statement();
+    src = startPos;
+    old_src = startOldPos;
+    token = startToken;
 
-    while (symtab[symPointer - 1].layerNum == currentLayer) {
+    while (symtab[symPointer - 1].levelNum == currentlevel) {
         symPointer--;
     }
-    currentLayer--;
+    currentlevel--;
     return return_val;
 }
 
@@ -501,16 +546,17 @@ double function() {
 int main(int argc, char** argv)
 {
     int i, fd;
-    src = "array func else if return while print";
-    for (i = Array; i <= Print; ++i) {
+    src = "array func else if return while print puts read";
+    for (i = Array; i <= Read; ++i) {
         next();
         symtab[symPointer -1].type = i;
     }
-    if (!(src = old_src = malloc(POOLSIZE))) {
+    if (!(src = old_src = (char*)malloc(POOLSIZE))) {
         printf("could not malloc(%d) for source area\n", POOLSIZE);
         return -1;
     }
 
+    ++argv; --argc;
     if (argc > 0) {
         if (**argv == '-' && (*argv)[1] == 'd') {
             compileState = debug;
@@ -518,12 +564,11 @@ int main(int argc, char** argv)
         }
         else {
             compileState = run;
-            ++argv; --argc;
         }
     }
     if (argc < 1) {
         printf("usage: tryc [-d] file ...\n");
-        return 1;
+        return -1;
     }
 
     if ((fd = open(*argv, 0)) < 0) {                // read the source file
@@ -536,7 +581,11 @@ int main(int argc, char** argv)
     }
     src[i] = 0; // add EOF character
     close(fd);
-    eval();
+    next();
+    while (token != 0) {
+        statement();
+    }
+    return 0;
 }
 
 
